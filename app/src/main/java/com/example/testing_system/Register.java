2a0eba5d40aebf446ed0958e294c79091eeecb81
nastuaah <homenko.nastya2004@yandex.ru>
2023-05-05 11:59:39 +0300

    Register

--- app/src/main/java/com/example/testing_system/Register.java
+++ app/src/main/java/com/example/testing_system/Register.java
@@ -1,17 +1,17 @@
 package com.example.testing_system;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-public class Register extends AppCompatActivity {
+public class Register extends Activity {
     EditText Login, Password, Name, Surname, MiddleName, DateOfBirth, Email, MobileNumber, Question, Answer ;
     Button Register;
     String LoginHolder, PasswordHolder, NameHolder, SurnameHolder, MiddleNameHolder, DateOfBirthHolder, EmailHolder, MobileNumberHolder,  QuestionHolder, AnswerHolder;
