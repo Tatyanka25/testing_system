b64fe17785ff930bfab98ad2f1ca4aba62862429
Vadim Syrov <steamruler@mail.ru>
2023-05-06 21:07:19 -0700

    Refactor

--- app/src/main/java/com/example/testing_system/OptionActivity.java
+++ app/src/main/java/com/example/testing_system/OptionActivity.java
@@ -3,7 +3,6 @@ package com.example.testing_system;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class OptionActivity extends Activity {
@@ -15,65 +14,41 @@ public class OptionActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
-        btnOption1 = (Button) findViewById(R.id.idBtnOption1);
-        btnOption2 = (Button) findViewById(R.id.idBtnOption2);
-        btnOption3 = (Button) findViewById(R.id.idBtnOption3);
-        btnOption4 = (Button) findViewById(R.id.idBtnOption4);
-        btnFinalProject = (Button) findViewById(R.id.idBtnFinalTest);
+        btnOption1 = findViewById(R.id.idBtnOption1);
+        btnOption2 = findViewById(R.id.idBtnOption2);
+        btnOption3 = findViewById(R.id.idBtnOption3);
+        btnOption4 = findViewById(R.id.idBtnOption4);
+        btnFinalProject = findViewById(R.id.idBtnFinalTest);
 
-        btnFinalProject.setOnClickListener(new View.OnClickListener() {
-
-    @Override
-    public void onClick(View v) {
-         Intent intent = new Intent(OptionActivity.this, QuestionActivity.class);
-         startActivity(intent);
-         finish();
-            }
+        btnFinalProject.setOnClickListener(v -> {
+             Intent intent = new Intent(OptionActivity.this, QuestionActivity.class);
+             startActivity(intent);
+             finish();
         });
-        btnFinalProject.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(OptionActivity.this,
-                        FinalTestActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        btnFinalProject.setOnClickListener(v -> {
+            Intent intent = new Intent(OptionActivity.this, FinalTestActivity.class);
+            startActivity(intent);
+            finish();
         });
-        btnOption1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(OptionActivity.this,
-                        QuestionActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        btnOption1.setOnClickListener(v -> {
+            Intent intent = new Intent(OptionActivity.this, QuestionActivity.class);
+            startActivity(intent);
+            finish();
         });
-
-        btnOption2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(OptionActivity.this,
-                        QuestionActivity.class);
-                startActivity(intent);
-                finish();}
+        btnOption2.setOnClickListener(v -> {
+            Intent intent = new Intent(OptionActivity.this, QuestionActivity.class);
+            startActivity(intent);
+            finish();
         });
-
-        btnOption3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(OptionActivity.this,
-                        QuestionActivity.class);
-                startActivity(intent);
-                finish();}
+        btnOption3.setOnClickListener(v -> {
+            Intent intent = new Intent(OptionActivity.this, QuestionActivity.class);
+            startActivity(intent);
+            finish();
         });
-
-        btnOption4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(OptionActivity.this,
-                        QuestionActivity.class);
-                startActivity(intent);
-                finish();}
+        btnOption4.setOnClickListener(v -> {
+            Intent intent = new Intent(OptionActivity.this, QuestionActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
