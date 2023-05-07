cb1e9b9d5302fe90c0f9add12011af52992ca750
nastuaah <homenko.nastya2004@yandex.ru>
2023-05-07 11:44:27 +0300

    Creating Question Database

---
+++ app/src/main/java/com/example/testing_system/CategoryActivity.java
@@ -0,0 +1,38 @@
+package com.example.testing_system;
+
+public class CategoryActivity {
+    public static final int ANIMALS = 1;
+    public static final int PEOPLE = 2;
+    public static final int RANDOM = 3;
+
+    private int id;
+    private String name;
+
+    public CategoryActivity() {
+    }
+
+    public CategoryActivity(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
