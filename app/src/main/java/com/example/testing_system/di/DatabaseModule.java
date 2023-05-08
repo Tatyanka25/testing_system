3a26a6fd93b6eecc266cf1314bfb8b23cfa33273
Vadim Syrov <steamruler@mail.ru>
2023-05-07 18:31:27 -0700

    Add prepopulation logic

--- app/src/main/java/com/example/testing_system/di/DatabaseModule.java
+++ app/src/main/java/com/example/testing_system/di/DatabaseModule.java
@@ -3,9 +3,10 @@ package com.example.testing_system.di;
 import android.content.Context;
 
 import androidx.room.Room;
-
 import com.example.testing_system.database.AppDatabase;
+import com.example.testing_system.models.SecurityQuestion;
 import com.example.testing_system.repositories.QuestionRepository;
+import com.example.testing_system.repositories.SecurityQuestionRepository;
 import com.example.testing_system.repositories.UserRepository;
 import dagger.Module;
 import dagger.Provides;
@@ -18,10 +19,20 @@ import dagger.hilt.android.qualifiers.ApplicationContext;
 public class DatabaseModule {
     @Provides
     public static AppDatabase provideAppDatabase(@ApplicationContext Context context) {
-        return Room
+        AppDatabase db = Room
                 .databaseBuilder(context, AppDatabase.class, "testing_system")
                 .allowMainThreadQueries()
                 .build();
+        if (!db.securityQuestionDao().any()) prepopulate(db);
+        return db;
+    }
+
+    private static void prepopulate(AppDatabase db) {
+        db.securityQuestionDao().insert(
+                new SecurityQuestion("Option 1"),
+                new SecurityQuestion("Option 2"),
+                new SecurityQuestion("Option 3")
+        );
     }
 
     @Provides
