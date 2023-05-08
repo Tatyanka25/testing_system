90ab424c723440b4ccd223e08c7f41035aeff25f
Vadim Syrov <steamruler@mail.ru>
2023-05-07 20:05:21 -0700

    Set up datepicker

--- app/src/main/java/com/example/testing_system/RegisterActivity.java
+++ app/src/main/java/com/example/testing_system/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.testing_system;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -13,8 +15,11 @@ import com.example.testing_system.models.User;
 import com.example.testing_system.repositories.SecurityQuestionRepository;
 import com.example.testing_system.repositories.UserRepository;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
-import java.util.Date;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Locale;
 import javax.inject.Inject;
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -27,8 +32,10 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputLayout Login, Password, Name, Surname, MiddleName, DateOfBirth, Email, MobileNumber, Question, Answer ;
     Button Register;
     String LoginHolder, PasswordHolder, NameHolder, SurnameHolder, MiddleNameHolder, DateOfBirthHolder, EmailHolder, MobileNumberHolder,  QuestionHolder, AnswerHolder;
+    private TextInputEditText DateOfBirthEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Login = findViewById(R.id.idEditUserName);
@@ -37,6 +44,17 @@ public class RegisterActivity extends AppCompatActivity {
         Surname = findViewById(R.id.idEditSurname);
         MiddleName = findViewById(R.id.idEditMiddleName);
         DateOfBirth = findViewById(R.id.idEditDateOfBirth);
+        DateOfBirthEditText = findViewById(R.id.idEditTextDateOfBirth);
+        DateOfBirthEditText.setOnClickListener(v -> {
+            Calendar now = Calendar.getInstance();
+            DatePickerDialog dialog = new DatePickerDialog(this, (view, year, month, dayOfMonth) -> {
+                now.set(Calendar.YEAR, year);
+                now.set(Calendar.MONTH, month);
+                now.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                DateOfBirthEditText.setText(dateFormatter.format(now.getTime()));
+            }, now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH));
+            dialog.show();
+        });
         Email = findViewById(R.id.idEditEmail);
         MobileNumber = findViewById(R.id.idEditPhoneNumber);
         Question = findViewById(R.id.idEditSecurityQuestion);
@@ -44,21 +62,25 @@ public class RegisterActivity extends AppCompatActivity {
         Answer = findViewById(R.id.idEditQuestionAnswer);
         Register = findViewById(R.id.idBtnRegister);
         Register.setOnClickListener(view -> {
-            MoveToLocalStrings();
-            if (!Validate()) {
-                Snackbar.make(view, R.string.validation_error, Snackbar.LENGTH_SHORT).show();
-                return;
+            try {
+                MoveToLocalStrings();
+                if (!Validate()) {
+                    Snackbar.make(view, R.string.validation_error, Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (repository.checkIfUserWithEmailExists(Email.getEditText().getText().toString())) {
+                    Snackbar.make(view, R.string.email_already_exists, Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                User user = new User(LoginHolder, EncryptionHelper.toSHA256String(PasswordHolder),
+                        NameHolder, MiddleNameHolder, SurnameHolder, EmailHolder, MobileNumberHolder,
+                        dateFormatter.parse(DateOfBirthHolder), QuestionHolder, AnswerHolder);
+                repository.insert(user);
+                Snackbar.make(view, R.string.reg_success, Snackbar.LENGTH_SHORT).show();
+                finish();
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
             }
-            if (repository.checkIfUserWithEmailExists(Email.getEditText().getText().toString())) {
-                Snackbar.make(view, R.string.email_already_exists, Snackbar.LENGTH_SHORT).show();
-                return;
-            }
-            User user = new User(LoginHolder, EncryptionHelper.toSHA256String(PasswordHolder),
-                    NameHolder, MiddleNameHolder, SurnameHolder, EmailHolder, MobileNumberHolder,
-                    new Date(1000), QuestionHolder, AnswerHolder);
-            repository.insert(user);
-            Snackbar.make(view, R.string.reg_success, Snackbar.LENGTH_SHORT).show();
-            finish();
         });
     }
 
